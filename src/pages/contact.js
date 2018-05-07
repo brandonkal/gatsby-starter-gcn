7457f5ae1724e26ee321ae1ed52be86ef8548d48
Brandon Kalinowski <bwebservice@gmail.com>
2018-05-07 02:08:03 -0700

    Finished SEO!!!!
    To Do:
    1. Squash Commits
    2. Rebase onto upstream master
    3. Pull Request
    4. Fix Footer Jump

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,6 @@ import config from '../utils/siteConfig'
 import Container from '../components/Container'
 import PageTitle from '../components/PageTitle'
 import ContactForm from '../components/ContactForm'
-import SEO from '../components/SEO'
 
 const Contact = ({ data }) => {
   return (
